777abd3607989bcd1c6f6e5ca02313c07d30a6e5
aidanhopper <aidanhop1@gmail.com>
2021-10-09 19:50:09 -0700

    preanybar

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 30;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 20;       /* vertical padding of bar */
+static const int sidepad            = 30;       /* horizontal padding of bar */
 static const int barheight          = 15;
 static const char *fonts[]          = { "CaskaydiaCove Nerd Font Mono:size=16" };
 static const char col_gray1[]       = "#2E3440";
